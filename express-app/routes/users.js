ac6d3b60db395033c8ccad962bf1b1082cbf63d9
Тимур Файзуллин <aaa.942006@gmail.com>
2026-03-24 19:08:31 +0500

    6 done

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
-
+let users = [{
+  "id": 1,
+  "name": "name"
+}];
+let inc = 2;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+  res.send(users);
 });
-
+router.post('/', function(req, res, next) {
+  const newUser = req.body;
+  users.push({"id" : inc, "name" : req.body.name});
+  inc++;
+  res.status(201).json(newUser);
+});
+router.get('/:id', function(req, res, next) {
+  const id = parseInt(req.params.id);
+  const user = users.find(user => user.id === id);
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).json({ error: 'User not found' });
+  }
+})
 module.exports = router;
